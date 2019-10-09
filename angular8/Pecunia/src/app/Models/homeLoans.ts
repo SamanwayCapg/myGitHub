0cd482c0942d5e81b5fdd1ce26014fe1f04c4b27
SamanwayCapg <54560571+SamanwayCapg@users.noreply.github.com>
2019-10-09 23:43:02 +0530

    pecunia angular

---
+++ angular8/Pecunia/src/app/Models/homeLoans.ts
@@ -0,0 +1,47 @@
+export class HomeLoan {
+
+  id: number;
+  loanID: string;
+  customerID: string;
+  amountApplied: number;
+  interestRate: number;
+  EMI_amount: number;
+  reapaymentPeriod: number;
+  dateOfApplication: string;
+  status: string;
+  occupation: string;
+  serviceYears: number;
+  grossIncome: number;
+  salaryDeductions: number;
+
+  constructor(ID: number,
+    LoanID: string,
+    CustomerID: string,
+    AmountApplied: number,
+    InterestRate: number,
+    EMI_Amount: number,
+    RepaymentPeriod: number,
+    DateOfApplication: string,
+    Status: string,
+    Occupation: string,
+    ServiceYears: number,
+    GrossIncome: number,
+    SalaryDeductions: number)
+  {
+    this.id = ID;
+    this.loanID = LoanID;
+    this.customerID = CustomerID;
+    this.amountApplied = AmountApplied;
+    this.interestRate = InterestRate;
+    this.EMI_amount = EMI_Amount;
+    this.reapaymentPeriod = RepaymentPeriod;
+    this.dateOfApplication = DateOfApplication;
+    this.status = Status;
+
+    this.occupation = Occupation;
+    this.serviceYears = ServiceYears;
+    this.grossIncome = GrossIncome;
+    this.salaryDeductions = SalaryDeductions;
+  }
+
+}
---
+++ src/app/Services/eduLoans.services.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { EduLoan } from '../Models/eduLoans'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EduLoanService
+{
+  constructor(private httpClient: HttpClient)
+  {
+
+  }
+
+  ApplyEduLoan(neweduLoan: EduLoan): Observable<boolean> {
+    
+  }
+}
